beab684b2967b57dd660954fce8d01029cda8a42
solopointer <solopointer1202@gmail.com>
2020-03-23 15:57:10 +0800

    upload

--- 648.Replace_Words/solution.cpp
+++ 648.Replace_Words/solution.cpp
@@ -2,10 +2,29 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <memory>
 #include "trie.h"
 using namespace std;
+
 class Solution {
 public:
+    string process(string& word, trie& tr) {
+        auto pos = tr.root;
+        size_t len = 1;
+        for (auto i : word) {
+            if (pos->children[i - 'a']) {
+                if (pos->children[i - 'a']->is_word) {
+                    return word.substr(0, len);
+                } else {
+                    pos = pos->children[i - 'a'];
+                }
+            } else {
+                break;
+            }
+            ++len;
+        } 
+        return word;
+    }
     string replaceWords(vector<string>& dict, string sentence) {
         std::stringstream ss;
         trie tr;
@@ -18,17 +37,18 @@ public:
                 ++end;
             } while(sentence[end] != ' ' && end < sentence.size());
             auto word = sentence.substr(begin, end - begin);
+            ss << process(word, tr)<< " ";
             begin = end + 1;
         }
-        return "asdf";
+        return ss.str().substr(0, ss.str().size() - 1);
     }
 };
 
 
 int main() {
-    vector<string> dict = {"cat", "bat", "rat", "ca"};
+    vector<string> dict = {"cat", "bat", "rat"};
     Solution s;
-    s.replaceWords(dict, "the cattle was rattled by the battery");
+    std::cout<<s.replaceWords(dict, "the cattle was rattled by the battery");
 
     return 0;
 }
